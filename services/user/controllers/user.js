5230be06a2ac870c6ead2e20e80d709f9f8f53be
Ryan Auger <ryanaugercc@gmail.com>
2019-08-07 16:40:23 -0400

    User POST & GET functional

--- services/user/controllers/user.js
+++ services/user/controllers/user.js
@@ -1,31 +1,72 @@
-const Joi = require('Joi');
-const {User} = require('../../../lib/models/userModel');
-const {parseAndValidate}= require('../../../lib/handlers/bodyParser');
+const Joi = require('joi');
+const {User} = require('../lib/models/userModel');
+const {parseAndValidate}= require('../lib/handlers/bodyParser');
 
-// POST Body format validator
+
+/*
+ * Register
+ */
+// Register Body Format Validator
 const schema = Joi.object().keys({
   first_name: Joi.string().required(),
   last_name: Joi.string().required(),
-  email: Joi.email().required(),
+  email: Joi.string().required(),
   password: Joi.string().required(),
 });
 
 async function register(event) {
-  const response = {};
+  let response = {statusCode: 400};
+  console.log('Event: ', event);
   try {
-    console.log('Event: ', event);
-
     // Parse and validate payload
     const body = parseAndValidate(event.body, schema);
+    console.log(body)
 
     // Insert the user into the database
-    const test = User.query().insert({
+    const user = await User.query().insert({
       first_name: body.first_name,
       last_name: body.last_name,
       email: body.email,
       password: body.password,
     });
-    console.log('test: ', test);
+
+    // Build reply
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'success',
+        user_id: user.id,
+      }),
+    };
+    console.log('test: ', user);
+  } catch (err) {
+    console.log('Error: ', err);
+    response.body = JSON.stringify({message: 'Failed to register'});
+  }
+  return response;
+}
+
+
+/*
+*  GET a user
+*/
+async function fetch(event) {
+  console.log(event);
+  let response = {statusCode: 500};
+  try {
+    const userId = event['pathParameters']['user_id'];
+    console.log('userId: ', userId);
+    const user = await User.query().findById(userId);
+    // Don't return the password
+    user.password = null;
+
+    console.log(user);
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        result: user,
+      }),
+    };
   } catch (err) {
     console.log(err);
   }
@@ -34,4 +75,6 @@ async function register(event) {
 
 module.exports = {
   register: register,
+  fetch: fetch,
 };
+
--- services/user/controllers/wallet.js
+++ services/user/controllers/wallet.js
@@ -1,13 +1,13 @@
 
 // POST Body format validator
-const schema = Joi.object().keys({
-  address_1: Joi.string().required(),
-  address_2: Joi.string().required(),
-  city: Joi.string().required(),
-  state: Joi.string().required(),
-  zip: Joi.string().required(),
-  ssn: Joi.number().integer().required(),
-});
+// const schema = Joi.object().keys({
+//   address_1: Joi.string().required(),
+//   address_2: Joi.string().required(),
+//   city: Joi.string().required(),
+//   state: Joi.string().required(),
+//   zip: Joi.string().required(),
+//   ssn: Joi.number().integer().required(),
+// });
 
 // Accepts the KYC information from the user.
 // If the user exists, and does not have any active account,
