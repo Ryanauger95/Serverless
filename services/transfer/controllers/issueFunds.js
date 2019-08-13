1ffd4a0dae3a4efbd3dc56f9abeca6d437e9b0ea
Ryan Auger <ryanaugercc@gmail.com>
2019-08-13 09:10:03 -0400

    Saving bc intellisense stopped working properly for exported modules

--- services/transfer/controllers/issueFunds.js
+++ services/transfer/controllers/issueFunds.js
@@ -1,5 +1,14 @@
+const WalletModel = require('../lib/models/walletModel');
+const SilaWallet = WalletModel.SilaWallet;
+const {Txn} = require('../lib/models/txn');
 
-
-function issueAll() {
-
+async function issueAll() {
+  console.log(Txn);
+  const res = await Txn.query().findById(22);
+  console.log(res);
 }
+
+module.exports = {
+  issueAll,
+}
+;
